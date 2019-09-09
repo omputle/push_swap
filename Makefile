ef9f9a20aa8d67dff923f7052a856817e41b6875
Orapeleng MPUTLE <omputle@c4r12s5.wethinkcode.co.za>
2019-09-09 17:31:16 +0200

    Push Swap

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: omputle <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/07/27 02:52:32 by omputle           #+#    #+#              #
-#    Updated: 2019/09/09 17:21:15 by omputle          ###   ########.fr        #
+#    Updated: 2019/09/09 17:29:54 by omputle          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -36,6 +36,7 @@ SRC = rules/rotate.c \
 	  find_max.c \
 	  mid_point.c \
 	  sort.c \
+	  push_swap.c \
 	  sort_three.c \
 	  sort_five.c \
 	  get_next_line.c \
@@ -51,10 +52,10 @@ test: $(SRC)
 	$(CC) $(SRC) $(FLAGS) libft/libft.a linked_lists/linked_lists.a rules/rules.a -o test
 
 fclean:
-	rm -r test
 	make -C libft/ fclean
 	make -C linked_lists/ fclean
 	make -C rules/ fclean
+	rm -r test
 
 norm:
 	norminette -R CheckForbiddenSourceHeader
--- sort_five.c
+++ sort_five.c
@@ -6,24 +6,13 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/07 06:40:55 by omputle           #+#    #+#             */
-/*   Updated: 2019/09/07 07:05:32 by omputle          ###   ########.fr       */
+/*   Updated: 2019/09/09 17:27:21 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int		mid_point(int n)
-{
-	int	temp;
-
-	temp = 0;
-	if (n % 2 != 0)
-		n = n + 1;
-	temp = n / 2;
-	return (temp);
-}
-
-static void	find_min(t_node **stack_A)
+static void	find_min_val(t_node **stack_A)
 {
 	t_node	*ptr;
 	int		data;
@@ -71,9 +60,9 @@ static void	find_min(t_node **stack_A)
 
 void	sort_five(t_node **stack_a, t_node **stack_b)
 {
-	find_min(stack_a);
+	find_min_val(stack_a);
 	pb(stack_a, stack_b);
-	find_min(stack_a);
+	find_min_val(stack_a);
 	pb(stack_a, stack_b);
 	sort_three(stack_a);
 	pa(stack_b, stack_a);
