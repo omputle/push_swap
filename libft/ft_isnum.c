46da596ad01f0e3d0cf23fc86f27f945e14623e2
Orapeleng MPUTLE <omputle@c4r12s5.wethinkcode.co.za>
2019-09-11 16:38:36 +0200

    Push Swap

--- libft/ft_isnum.c
+++ libft/ft_isnum.c
@@ -6,7 +6,7 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/29 15:20:40 by omputle           #+#    #+#             */
-/*   Updated: 2019/09/04 11:51:21 by omputle          ###   ########.fr       */
+/*   Updated: 2019/09/11 08:56:47 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int		ft_isnum(char *str)
 	int	count;
 
 	count = 0;
+	if ((str[count] == '+' || str[count] == '-') && ft_strlen(str) > 0)
+		count++;
 	while (str[count] != '\0')
 	{
-		if (str[count] == '+' || str[count] == '-')
-			count++;
 		if (ft_isdigit(str[count]) == 0)
 			return (0);
 		count++;
