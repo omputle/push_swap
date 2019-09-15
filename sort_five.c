d790796979f7c19192173e827f84f6020b275330
Orapeleng MPUTLE <omputle@c4r12s5.wethinkcode.co.za>
2019-09-15 10:17:44 +0200

    Push Swap

--- sort_five.c
+++ sort_five.c
@@ -6,12 +6,13 @@
 /*   By: omputle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/07 06:40:55 by omputle           #+#    #+#             */
-/*   Updated: 2019/09/14 13:56:56 by omputle          ###   ########.fr       */
+/*   Updated: 2019/09/15 10:17:22 by omputle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/*This function locates the minimum value in stack_a and moves it to the top of the stack*/
 static void	find_min_val(t_node **stack_a)
 {
 	t_node	*ptr;
@@ -41,12 +42,15 @@ static void	find_min_val(t_node **stack_a)
 
 void		sort_five(t_node **stack_a, t_node **stack_b)
 {
+	/*Finds the minimum value in stack_a and pushes it to stack_b*/
 	while (list_length(stack_a) > 3)
 	{
 		find_min_val(stack_a);
 		pb(stack_a, stack_b);
 	}
+	/*Sorts the remaining three nodes left in the stack*/
 	sort_three(stack_a);
+	/*Brings back the nodes from stack_b to stack_a*/
 	while (list_length(stack_b) > 0)
 		pa(stack_b, stack_a);
 }
